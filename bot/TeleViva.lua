417c54b9992aea1a9739c3517dc03c597d8c2271
ArianWatch <DAwshArian@gmail.com>
2016-03-19 19:39:02 +0330

    Update and rename TeleSpeed.lua to TeleViva.lua

--- bot/TeleViva.lua
+++ bot/TeleViva.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {146480433,129315952tonumber(our_id)},--Sudo users
+    sudo_users = {137984481},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[TeleSpeed v2 - Open Source
