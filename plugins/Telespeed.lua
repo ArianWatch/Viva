459e0f277b4649b2e974655bd6116f39957ff3d6
TeleSpeeds <amir_hossein39@yahoo.com>
2016-03-19 17:14:55 +0330

    Create Telespeed.lua

---
+++ plugins/Telespeed.lua
@@ -0,0 +1,43 @@
+do
+
+function run(msg, matches)
+  return 'TeleSpeed Telegram Bot v2'.. [[ 
+  
+TeleSpeed version 2
+
+
+                           Powere
+                           @mohamadtictic Mohamad
+
+                                                  sudo
+                                                  @mohamadtictic Mohamad 
+                                                  @Dehladeh4 Amir Hossein
+                                                  @ThisIsamirh Amir Hossein
+
+                                                                          Special Thanks to 
+                                                                          @Dehladeh4 amir Hossein 
+                                                                          @ThisIsamirh Amir Hossein
+                                                                                create Gego 
+                                                                                 @Mod999 Mohamad
+                                                                                                      admin 
+                                                                                                      @Dr_VIRUS Efran
+                                                                                                               chanel
+																							                   @telesped
+																											   ]]
+
+end
+
+return {
+  description = "Robot and Creator About", 
+  usage = "/ver : robot info",
+  patterns = {
+    "^[!/]ver$",
+	"^[Tt]elespeed",
+	"^[!/]telespeed",
+	"^[!/]Telespeed",
+	"^[!/]version"
+  }, 
+  run = run 
+}
+
+end
